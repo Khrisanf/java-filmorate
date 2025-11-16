663a22ac105d0ea8ccc18acca8fe04051d78545f
Petrichor <vikachris2007@gmail.com>
2025-11-16 19:16:16 +0300

    test: add popularity tests (тяжело....), checkstyle

--- src/test/java/ru/yandex/practicum/filmorate/service/FilmSearchServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/FilmSearchServiceTest.java
@@ -14,7 +14,8 @@ import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class FilmSearchServiceTest {
 
