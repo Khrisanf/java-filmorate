28ff472b33f37f775eca5743e67e3cc31c31fe6f
Alexey.R <VectorID1@yandex.ru>
2025-11-19 15:40:29 +0300

    Реализация ленты событий Добавили ленту событий которая выводит последние действия пользователя с id по get запросу. Так же добавили поля в UserService, FilmService, ReviewService так как методы добавления в ленту зависят напрямую от методов вызываемых в этих сервисах. Так же добавили новую таблицу которая хранит эту ленту событий. Покрыли тестыми FeedService.

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -8,6 +8,7 @@ import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 import ru.yandex.practicum.filmorate.model.event.EventOperation;
 import ru.yandex.practicum.filmorate.model.event.EventType;
 import ru.yandex.practicum.filmorate.model.film.Film;
+import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.model.film.Genre;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
