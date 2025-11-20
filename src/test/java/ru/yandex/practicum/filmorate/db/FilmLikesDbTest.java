5d7938061a6c6cd58e6523829bb688b64eb4364d
Petrichor <vikachris2007@gmail.com>
2025-11-20 19:03:33 +0300

    patch: checkstyle

--- src/test/java/ru/yandex/practicum/filmorate/db/FilmLikesDbTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/db/FilmLikesDbTest.java
@@ -25,7 +25,6 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.CollectionAssert.assertThatCollection;
 
 
 @JdbcTest
