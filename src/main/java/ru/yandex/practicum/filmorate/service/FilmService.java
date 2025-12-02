d2464e921d1e252bf3c43cb4af42266daae75638
arsenijprokofev <arseniyprokofev17279@gmail.com>
2025-12-02 12:05:43 +0300

    обновил director

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -229,26 +229,21 @@ public class FilmService {
             return List.of();
         }
         List<Film> films = filmStorage.search(query, searchBy);
+        films.forEach(film -> {
+            Set<Director> validDirectors = new HashSet<>();
 
-        // Фильтруем фильмы, у которых есть только удаленные режиссеры
-        films = films.stream()
-                .filter(film -> hasExistingDirectors(film))
-                .collect(Collectors.toList());
+            for (Director director : film.getDirectors()) {
+                try {
+                    Director validDirector = directorService.findById(director.getId());
+                    validDirectors.add(validDirector);
+                } catch (NotFoundException e) {
+                    log.debug("Director {} is deleted, filtering out", director.getId());
+                }
+            }
 
-        films.forEach(f -> log.info(
-                "Search result film id={}, name={}, likes={}, date={}",
-                f.getId(), f.getName(), f.getLikes().size(), f.getReleaseDate()
-        ));
+            film.setDirectors(validDirectors);
+        });
 
         return films;
-    }
-
-    private boolean hasExistingDirectors(Film film) {
-        if (film.getDirectors() == null || film.getDirectors().isEmpty()) {
-            return true;
-        }
-
-        return film.getDirectors().stream()
-                .anyMatch(director -> director.getName() != null);
     }
 }
